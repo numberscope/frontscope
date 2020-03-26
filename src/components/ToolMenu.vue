05332b6b952311852c32b05b4193f7a74d59f832
Khaled Allen <khaled.allen@gmail.com>
2020-03-26 12:20:46 -0600

    wip

--- src/components/ToolMenu.vue
+++ src/components/ToolMenu.vue
@@ -1,12 +1,21 @@
 <template>
+  <div>
     <ul class="list-group">
         <ToolSelector
             v-for="tool in tools"
             v-bind:title="tool.name"
             v-bind:key="tool.id"
+            v-on:set-active-tool="activeTool = $event"
             >
             </ToolSelector>
     </ul>
+    <div v-if="activeTool !== null">
+      <p>Active Tool Selected</p>
+      <VizualizationSettingsPane
+        v-bind:vizParams="activeTool.params">
+      </VizualizationSettingsPane>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,14 +23,24 @@ import ToolSelector from './ToolSelector.vue'
 import Scope from '../global/ScopeState.js'
 
 export default {
-name: 'ToolMenu',
-components: {
-                ToolSelector
-            },
-data: function(){
-  return{
-    tools: Scope.modules,
-  }
+  name: 'ToolMenu',
+  components: {
+    ToolSelector,
+    VizualizationSettingsPane
+  },
+  methods: {
+    setActive: function(active) {
+        console.log(active);
+        const moduleKey = 'module' + active.replace(' ','');
+        Scope.state.activeViz = Scope.modules[moduleKey]
+        console.log(Scope.state.activeViz);
+        }
+  },
+  data: function(){
+    return{
+      tools: Scope.modules,
+      activeTool: null
+    }
   }
 }
 
--- src/components/ToolSelector.vue
+++ src/components/ToolSelector.vue
@@ -1,24 +1,15 @@
 <template>
-    <a v-on:click="setActive(title)"><li class="list-group-item">{{ title }}</li></a>
+    <a v-on:click="$emit('set-active-tool', title)"><li class="list-group-item">{{ title }}</li></a>
 </template>
 
 <script>
-import Scope from '../global/ScopeState.js';
 
 export default {
     name: 'ToolSelector',
 
     props: {
         title: String,
-    },
-    methods: {
-        setActive: function(active) {
-            console.log(active);
-            const moduleKey = 'module' + active.replace(' ','');
-            Scope.state.activeViz = Scope.modules[moduleKey]
-            console.log(Scope.state.activeViz);
-            }
-        }
+    }
 }
 </script>
 
