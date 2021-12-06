544bf8573f5bcb636fd4336507c4b5b060edd57f
Kate Stange <kstange@math.colorado.edu>
2021-12-06 08:25:06 -0700

    SECRET #309

--- src/visualizers/VisualizerChaos.ts
+++ src/visualizers/VisualizerChaos.ts
@@ -117,48 +117,28 @@ class VisualizerChaos extends VisualizerDefault implements VisualizerInterface {
 
 	private img: p5.Image = new p5.Image();
 
-	// current state variables
-	private myIndex = 0;
-	private myTerm = 0;
-	private myCorner = 0;
-	private myCornerPosition: position = { x:0, y:0 };
-	private myWalker = 0;
-	// p5 colours can't be initialized outside draw() or setup()
-	private myColor: any; // eslint-disable-line @typescript-eslint/no-explicit-any 
-	private pixelCount = 0;
-
-	// private properly typed versions of the parameters
+	// private properly typed versions of the user parameters
 	private num = 0;
 	private corners = 0;
 	private walkers = 0;
 	private style = 0;
+	private highlightWalker = 0;
 	private circSize = 0;
 	private alpha = 1;
 	private frac = 0;
 	private pixelsPerFrame = 0;
 	private showLabels = false;
 	private darkMode = false;
-
-	// variables for the polygon size/layout/walkers
-	private ctrX = 0;
-	private ctrY = 0;
-	private radius = 0;
+	
+	// current state variables (used in setup and draw)
+	private myIndex = 0;
+	private pixelCount = 0;
 	private cornersList: position[] = [];
-	private cornersLabels: position[] = [];
 	private walkerPositions: position[] = [];
-	private highlightWalker = 0;
-
-	// text control (these values are overwritten in setup)
-	private labelOutset = 1.1;
-	private textSize = 15;
-	private textStroke = 1; 
 
 	// list of colour palettes
 	private palettes: palette[] = [];
 	private currentPalette: palette = { colorList: [], backgroundColor: this.sketch.color(255), textColor: this.sketch.color(20) };
-	private colorList: any[] = []; // eslint-disable-line @typescript-eslint/no-explicit-any 
-	private backgroundColor: any; // eslint-disable-line @typescript-eslint/no-explicit-any 
-	private textColor: any; // eslint-disable-line @typescript-eslint/no-explicit-any 
 	private basicAlpha = 0.5;
 
 	constructor() {
@@ -299,26 +279,18 @@ class VisualizerChaos extends VisualizerDefault implements VisualizerInterface {
 
 		// set the palette up
 		this.currentPalette = this.palettes[paletteIndex];
-		this.colorList = this.currentPalette.colorList;
-		this.backgroundColor = this.currentPalette.backgroundColor;
-		this.textColor = this.currentPalette.textColor;
-
-		// set initial values
-		this.myIndex = 0;
-		this.myCorner = 0;
-		this.myWalker = 0;
-		this.pixelCount = 0;
-		this.myColor = this.sketch.color('#003f5c');
+		const backgroundColor = this.currentPalette.backgroundColor;
+		const textColor = this.currentPalette.textColor;
 
 		// set center coords and size
-		this.ctrX = this.sketch.width / 2;
-		this.ctrY = this.sketch.height / 2;
-		this.radius = this.sketch.width / 3;
+		const ctrX = this.sketch.width / 2;
+		const ctrY = this.sketch.height / 2;
+		const radius = this.sketch.width / 3;
 
 		// text control
-		this.labelOutset = 1.1;
-		this.textSize = this.sketch.width * 0.1;
-		this.textStroke = this.sketch.width * 0.01;
+		const labelOutset = 1.1;
+		const textSize = this.sketch.width * 0.1;
+		const textStroke = this.sketch.width * 0.01;
 
 		// set counters
 		this.pixelCount = 0;
@@ -327,29 +299,29 @@ class VisualizerChaos extends VisualizerDefault implements VisualizerInterface {
 		// set up arrays of walkers
 		this.walkerPositions.splice(0, this.walkerPositions.length) // clean the array out (fixes bug with redraws)
 		for( let w = 0; w < this.walkers; w++ ){
-			this.walkerPositions.push({ x: this.ctrX, y: this.ctrY }); // all walkers start at origin
+			this.walkerPositions.push({ x: ctrX, y: ctrY }); // all walkers start at origin
 		}
 
 		// Set up the windows and return the coordinates of the corners
-		this.cornersList = this.chaosWindow(this.ctrX,this.ctrY,this.radius); // locations of the corners
+		this.cornersList = this.chaosWindow(ctrX, ctrY, radius); // locations of the corners
 
 		// Set frame rate
 		this.sketch.frameRate(10);
 
 		// canvas clear/background
 		this.sketch.clear();
-		this.sketch.background(this.backgroundColor);
+		this.sketch.background(backgroundColor);
 
 		// Draw corner labels if desired
 		if ( this.showLabels ) {
-			this.sketch.stroke(this.textColor);
-			this.sketch.fill(this.textColor);
-			this.sketch.strokeWeight(0);
-			this.sketch.textSize(15);
+			this.sketch.stroke(textColor);
+			this.sketch.fill(textColor);
+			this.sketch.strokeWeight(textStroke);
+			this.sketch.textSize(textSize);
 			this.sketch.textAlign(this.sketch.CENTER,this.sketch.CENTER);
-			this.cornersLabels = this.chaosWindow(this.ctrX,this.ctrY,(this.radius)*(this.labelOutset)); // locations of the labels
+			const cornersLabels = this.chaosWindow(ctrX, ctrY,(radius)*(labelOutset)); // locations of the labels
 			for ( let c = 0 ; c < this.corners ; c++ ) {
-				const label = this.cornersLabels[c];
+				const label = cornersLabels[c];
 				this.sketch.text(String(c),label.x,label.y);
 			}
 		}
@@ -360,6 +332,7 @@ class VisualizerChaos extends VisualizerDefault implements VisualizerInterface {
 
 	draw() {
 
+
 		// we do pixelsPerFrame pixels each time through the draw cycle; this speeds things up essentially
 		for( let px = 0; px < this.pixelsPerFrame; px++ ){
 
@@ -369,45 +342,46 @@ class VisualizerChaos extends VisualizerDefault implements VisualizerInterface {
 				this.myIndex += 1;
 
 				// get the term
-				this.myTerm = this.seq.getElement(this.myIndex);
+				const myTerm = this.seq.getElement(this.myIndex);
 
 				// check its modulus to see which corner to walk toward
-				this.myCorner = this.numModulus(this.myTerm, this.corners);
-				this.myCornerPosition = this.cornersList[this.myCorner];
+				const myCorner = this.numModulus(myTerm, this.corners);
+				const myCornerPosition = this.cornersList[myCorner];
 
 				// check the index modulus to see which walker is walking
-				this.myWalker = this.numModulus(this.myIndex,this.walkers);
+				const myWalker = this.numModulus(this.myIndex, this.walkers);
 
 				// update the walker position
-				this.walkerPositions[this.myWalker].x = (1-this.frac)*this.walkerPositions[this.myWalker].x + this.frac*this.myCornerPosition.x;
-				this.walkerPositions[this.myWalker].y = (1-this.frac)*this.walkerPositions[this.myWalker].y + this.frac*this.myCornerPosition.y;
+				this.walkerPositions[myWalker].x = (1-this.frac)*this.walkerPositions[myWalker].x + this.frac*myCornerPosition.x;
+				this.walkerPositions[myWalker].y = (1-this.frac)*this.walkerPositions[myWalker].y + this.frac*myCornerPosition.y;
 			
 				// choose colour to mark position
+				let myColor = this.sketch.color(0);
 				if( this.style == 0 ){ // colour by walker
-					this.myColor = this.colorList[this.myWalker];
+					myColor = this.currentPalette.colorList[myWalker];
 				}
 				if( this.style == 1 ){ // colour by destination
-					this.myColor = this.colorList[this.myCorner];
+					myColor = this.currentPalette.colorList[myCorner];
 				}
 				if( this.style == 2 ){ // colour by index
 					if ( this.num ) {
-						this.myColor = this.sketch.lerpColor(this.colorList[0], this.colorList[1], this.myIndex/this.num );
+						myColor = this.sketch.lerpColor(this.currentPalette.colorList[0], this.currentPalette.colorList[1], this.myIndex/this.num );
 					} else {
-						this.myColor = this.sketch.lerpColor(this.colorList[0], this.colorList[1], this.numModulus(this.myIndex,10000)/10000 );
+						myColor = this.sketch.lerpColor(this.currentPalette.colorList[0], this.currentPalette.colorList[1], this.numModulus(this.myIndex,10000)/10000 );
 					}
 				}
 				if( this.style == 3 ){ // colour one walker
-					if( this.myWalker == this.highlightWalker ){
-						this.myColor = this.colorList[0];
+					if( myWalker == this.highlightWalker ){
+						myColor = this.currentPalette.colorList[0];
 					} else {
-						this.myColor = this.colorList[1];
+						myColor = this.currentPalette.colorList[1];
 					}
 				}
-				this.myColor.setAlpha(255*this.alpha); // the 255 is needed when in RGB mode; can change in other modes; see p5.js docs on setAlpha
+				myColor.setAlpha(255*this.alpha); // the 255 is needed when in RGB mode; can change in other modes; see p5.js docs on setAlpha
 
 				// draw a circle
-				this.sketch.fill(this.myColor);
-				this.sketch.circle( this.walkerPositions[this.myWalker].x, this.walkerPositions[this.myWalker].y, this.circSize );
+				this.sketch.fill(myColor);
+				this.sketch.circle( this.walkerPositions[myWalker].x, this.walkerPositions[myWalker].y, this.circSize );
 
 				this.pixelCount += 1;
 			}
