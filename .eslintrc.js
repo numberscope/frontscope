ac70b445e806b128b977d8e566bb097bf0191c1d
Liam Mulhall <mulhall.edward.liam@gmail.com>
2022-01-07 16:27:52 -0700

    chore: Add Code Formatter
    Install and configure the Prettier code formatter. We also install the
    eslint-config-prettier package to get ESLint and Prettier to play nicely
    together, as recommended by the Prettier docs.
    
    For
    https://github.com/numberscope/frontscope/issues/26.
    Related to https://github.com/numberscope/frontscope/issues/69.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    'prettier'
   ],
   parserOptions: {
     ecmaVersion: 2020
