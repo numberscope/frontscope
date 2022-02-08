a008bd83faa9ee4c28bffafbc8d4a3f133c2dd08
Liam Mulhall <liam.mulhall@colorado.edu>
2022-02-07 22:40:31 -0700

    chore: Add Code Formatter (#90)
    chore: add code formatter
    
    Installs the `prettier-eslint` command in the package's 
    `node_modules/.bin` directory (well, it will be there in a new
    clone once `npm install` has been run). This command uses
    a combination of the `prettier` code formatter and `eslint --fix` to
    format the code to mostly align with the project's code style.
    
    Provides `npm` scripts to format and/or check all of the project's
    TypeScript or Vue files using the tool.
    
    Resolves #26.
    
    Install and configure the Prettier code formatter. We also install the
    eslint-config-prettier package to get ESLint and Prettier to play nicely
    together, as recommended by the Prettier docs.
    
    For
    https://github.com/numberscope/frontscope/issues/26.
    Related to https://github.com/numberscope/frontscope/issues/69.
    
    * chore: Change Prettier Rules
    
    Change arrow parens rule from "always" to "avoid" and change indent from
    2 to 4.
    
    For https://github.com/numberscope/frontscope/pull/90.
    
    * chore: Add Code Format Packages and ESLint Rule
    
    Add the prettier-eslint and prettier-eslint-cli packages. We also add
    the operator-linebreak ESLint rule.
    
    For https://github.com/numberscope/frontscope/pull/90.
    
    * chore: Add NPM Scripts for Formatting
    
    * docs: Add Doc About Formatting Code
    
    For https://github.com/numberscope/frontscope/pull/90.
    
    * style: Add Backticks
    
    * docs: Remove `package.json` Snippet
    
    We remove the snippet from `package.json` because there's already a
    detailed description of each snippet and we don't want to sync this
    manually when the relevant code changes.
    
    * docs: Fix Grammatical Issues
    
    * style: Add 80-Character ESLint Rule
    
    Enforce 80-character line limit for comments and code using ESLint.
    A line with more than 80 characters will produce an error when you
    run `npm run lint`, which uses vue-cli-service to run ESLint on each
    file it can in the project. Right now, there are 100+ lines that
    violate this rule.
    
    * doc: Add information about autorunning prettier-eslint in Emacs
    
    Co-authored-by: Glen Whitney <glen@studioinfinity.org>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,32 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'operator-linebreak': [2, 'before'],
+        'max-len': ['error', { 'code': 80, 'comments': 80, }]
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
 }
