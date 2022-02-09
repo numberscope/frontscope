0a3e19204bd9c08c8cde5a76c9b9598b9e2ff9a8
Liam Mulhall <liam.mulhall@colorado.edu>
2022-02-09 12:41:41 -0700

    fix: Make Footer Reappear, Make Footer Dynamic
    https://github.com/numberscope/frontscope/pull/101 made the footer
    disappear. In this commit, we make it reappear. We also make it dynamic
    so that it doesn't become outdated. We also add a test for the footer.
    
    I tried to add this commit in
    https://github.com/numberscope/frontscope/pull/101, but I must have
    merged it incorrectly.

--- src/views/minor/Footer.vue
+++ src/views/minor/Footer.vue
@@ -1,22 +1,29 @@
 <template>
-    <footer class="page-footer font-small" style="backgroundcolor: #3333ff">
+    <footer class="page-footer font-small">
         <div class="footer-copyright text-center py-3" style="color: white">
-            © 2020 Copyright:
-            <a style="color: white" href="https://colorado.edu">
-                colorado.edu
-            </a>
+            {{ copyright }}
         </div>
     </footer>
 </template>
 
 <script>
+    const currentYear = new Date().getFullYear()
+    const copyrightText
+        = `Copyright © 2020-${currentYear} University of Colorado Boulder`
     export default {
+        /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+        data() {
+            return {
+                copyright: copyrightText
+            }
+        },
         name: 'FooterComponent',
     }
 </script>
 
 <style>
     footer {
+        background-color: #3333ff;
         position: absolute;
         bottom: 0;
         width: 100%;
