54c66e7ef120047bbdd1fe755b09df502c3f9c4a
Liam Mulhall <liam.mulhall@colorado.edu>
2022-02-21 12:16:53 -0700

    feat: Add Husky
    Husky installs Git hooks. Add pre-commit hook that checks the formatting
    of the code base and runs unit tests.

--- src/components/icons/IconSupport.vue
+++ src/components/icons/IconSupport.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable -->
 <template>
     <svg
         xmlns="http://www.w3.org/2000/svg"
--- src/components/icons/IconTooling.vue
+++ src/components/icons/IconTooling.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable -->
 <!-- This icon is from <https://github.com/Templarian/MaterialDesign>, distributed under Apache 2.0 (https://www.apache.org/licenses/LICENSE-2.0) license-->
 <template>
     <svg
