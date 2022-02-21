9920240dda80ea638fcd1d3b9f480e276c10c4ce
Liam Mulhall <liam.mulhall@colorado.edu>
2022-02-21 11:26:21 -0700

    feat: Delete Everything

--- src/shared/ValidationStatus.ts
+++
@@ -1,9 +0,0 @@
-export class ValidationStatus {
-    public isValid: boolean
-    public errors: string[]
-
-    constructor(isValid: boolean, errors?: string[]) {
-        this.isValid = isValid
-        this.errors = errors !== undefined ? errors : []
-    }
-}
