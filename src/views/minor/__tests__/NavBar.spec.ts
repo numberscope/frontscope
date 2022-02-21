8227d046d61d80620059dada669f44e636af8f34
Liam Mulhall <liam.mulhall@colorado.edu>
2022-02-21 16:25:40 -0700

    test: Fix `NavBar.vue` Test Name

--- src/views/minor/__tests__/NavBar.spec.ts
+++ src/views/minor/__tests__/NavBar.spec.ts
@@ -2,7 +2,7 @@ import {mount} from '@vue/test-utils'
 import {expect, test} from 'vitest'
 import NavBar from '../NavBar.vue'
 
-test('something', async () => {
+test('should have links to home, scope, about, and help', async () => {
     const wrapper = mount(NavBar, {shallow: true})
     expect(wrapper.html()).toContain('/')
     expect(wrapper.html()).toContain('/scope')
