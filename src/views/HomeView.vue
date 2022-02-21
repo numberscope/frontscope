8bc2bf553bf8d1b0ecfd157064348cb404672e7d
Liam Mulhall <liam.mulhall@colorado.edu>
2022-02-21 13:23:42 -0700

    feat: Delete Boilerplate Vue Files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-    import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-    <main>
-        <TheWelcome />
-    </main>
+    <h1>Home</h1>
 </template>
