dec3619d2b6716beb36c230b6238915f0b87d9fd
Liam Mulhall <liam.mulhall@colorado.edu>
2022-03-07 13:20:23 -0700

    feat: Add SeqSelector Component and Test

---
+++ src/components/SeqSelector.vue
@@ -0,0 +1,35 @@
+<template>
+    <div v-if="!isInstance">
+        <a v-on:click="$emit('set-seq-params')">
+            <li class="list-group-item">{{ title }}</li>
+        </a>
+    </div>
+    <div v-else>
+        <a v-on:click="$emit('stage-instance')">
+            <li class="list-group-item">Custom: {{ title }}</li>
+        </a>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'SeqSelector',
+        props: {
+            title: String,
+            isInstance: Boolean,
+        },
+    }
+</script>
+
+<style scoped>
+    a {
+        cursor: pointer;
+    }
+    li:hover {
+        background-color: #809fff;
+        color: #ffffff;
+    }
+    li {
+        text-align: left;
+    }
+</style>
