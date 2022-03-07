5b68f29d5de7e1a36af2fbb886c3d527a1cae01f
Liam Mulhall <liam.mulhall@colorado.edu>
2022-03-07 12:53:19 -0700

    feat: Add SeqGetter Component and Test

---
+++ src/components/SeqGetter.vue
@@ -0,0 +1,28 @@
+<template>
+    <a v-on:click="$emit('load-seq')">
+        <li class="list-group-item">{{ title }}</li>
+    </a>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'SeqGetter',
+        props: {
+            title: String,
+            instanceId: String,
+        },
+    }
+</script>
+
+<style scoped>
+    a {
+        cursor: pointer;
+    }
+    li:hover {
+        background-color: #809fff;
+        color: #ffffff;
+    }
+    li {
+        text-align: left;
+    }
+</style>
