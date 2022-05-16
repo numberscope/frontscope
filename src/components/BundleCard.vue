e51a5a0f013a0cf31a3bf4890960f0bcfd566a4f
Liam Mulhall <liam.mulhall@colorado.edu>
2022-05-16 10:24:20 -0600

    feat: Add Placeholder Tests
    Add placeholder tests for:
      - VisualizationSettingsPane.vue
      - BundleCard.vue
    
    Neither of these files could be tested as they are, unfortunately.

---
+++ src/components/BundleCard.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="col-4-sm">
+        <div class="card" style="width: 18rem">
+            <div class="card-body">
+                <h5 class="card-title">{{ seq.name + ' + ' + viz.name }}</h5>
+                <p class="card-text">
+                    This is a bundle of {{ seq.name + ' + ' + viz.name }}.
+                </p>
+                <div :id="this.uid"></div>
+                <a
+                    v-on:click="$emit('drawBundle', {seq: seq, viz: viz})"
+                    href="#"
+                    class="btn btn-primary mr-2">
+                    Draw
+                </a>
+                <a
+                    v-on:click="$emit('removeBundle', {seq: seq, viz: viz})"
+                    href="#"
+                    class="btn btn-danger">
+                    Remove
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import p5 from 'p5'
+    export default {
+        name: 'BundleCard',
+        mounted() {
+            const seq = this.seq
+            seq.initialize()
+            const viz = this.viz
+            const thumb = new p5(function (sketch) {
+                viz.initialize(sketch, seq)
+                sketch.setup = function () {
+                    sketch.createCanvas(200, 200)
+                    sketch.background('white')
+                    viz.setup()
+                }
+                sketch.draw = function () {
+                    viz.draw()
+                    console.log(sketch.frameCount)
+                    if (sketch.frameCount >= 50) {
+                        sketch.noLoop()
+                    }
+                }
+            }, document.getElementById(this.uid) as HTMLElement)
+            thumb.setup()
+            thumb.draw()
+        },
+        methods: {},
+        props: {
+            seq: Object,
+            viz: Object,
+        },
+    }
+</script>
+
+<style scoped>
+    .card {
+        margin: 1em;
+        min-height: 250px;
+    }
+</style>
---
+++ src/components/__tests__/VisualizationSettingsPane.spec.ts
@@ -0,0 +1,14 @@
+/* eslint-disable max-len */
+
+import {describe} from 'vitest'
+
+describe('VisualizationSettingsPane', () => {
+    /*
+    As of 2022-05-16, I (Liam Mulhall) don't think VisualizationSettingsPane.vue
+    can be tested. See:
+    https://github.com/numberscope/frontscope/blob/main/src/components/VizualizationSettingsPane.vue // eslint-disable-line
+
+    This file is a placeholder.
+    */
+    console.log('add a test if/when possible')
+})
