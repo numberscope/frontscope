e4e3a9283cfd4d56969a60cb8304881e756d5449
Liam Mulhall <liam.mulhall@colorado.edu>
2022-05-16 15:11:26 -0600

    feat: Add TS File

---
+++ src/sequences/SequenceFormula.ts
@@ -0,0 +1,87 @@
+import {SequenceExportModule, SequenceExportKind} from './SequenceInterface'
+import {SequenceCached} from './SequenceCached'
+import * as math from 'mathjs'
+
+/**
+ *
+ * @class SequenceFormula
+ * extends SequenceCached to use mathjs to compute an arbitrary formula in terms
+ * of n. Currently all such formulas start at index 0 and have no limit, but
+ * those are both arbitrary choices; we might at some point want to allow
+ * either to be tailored.
+ */
+class SequenceFormula extends SequenceCached {
+    name = 'Formula: empty'
+    description = 'A sequence defined by a formula in n'
+    formula = 'n'
+    params = {
+        formula: {
+            value: this.formula,
+            displayName: 'Formula',
+            required: true,
+        },
+    }
+
+    private evaluator: math.EvalFunction
+
+    /**
+     *Creates an instance of SequenceFormula
+     * @param {*} sequenceID the sequence identifier of the sequence
+     */
+    constructor(sequenceID: number) {
+        super(sequenceID)
+        // It is mandatory to initialize the `evaluator` property here,
+        // so just use a simple dummy formula until the user provides one.
+        this.evaluator = math.compile(this.formula)
+    }
+
+    checkParameters() {
+        const status = super.checkParameters()
+
+        let parsetree = undefined
+        try {
+            parsetree = math.parse(this.params.formula.value)
+        } catch (err) {
+            status.isValid = false
+            status.errors.push(
+                'Could not parse formula: ' + this.params.formula.value
+            )
+            status.errors.push(err.message)
+            return status
+        }
+        const othersymbs = parsetree.filter(
+            (node, path, parent) =>
+                // prettier-ignore
+                node.type === 'SymbolNode'
+                && parent?.type !== 'FunctionNode'
+                && node.name !== 'n'
+        )
+        if (othersymbs.length > 0) {
+            status.isValid = false
+            status.errors.push(
+                "Only 'n' may occur as a free variable in formula.",
+                // prettier-ignore
+                "Please remove '"
+                + (othersymbs[0] as math.SymbolNode).name
+                + "'"
+            )
+        }
+        this.evaluator = parsetree.compile()
+        return status
+    }
+
+    initialize(): void {
+        super.initialize()
+        this.name = 'Formula: ' + this.formula
+    }
+
+    calculate(n: number) {
+        return BigInt(this.evaluator.evaluate({n: n}))
+    }
+}
+
+export const exportModule = new SequenceExportModule(
+    SequenceFormula,
+    'Sequence by Formula',
+    SequenceExportKind.FAMILY
+)
