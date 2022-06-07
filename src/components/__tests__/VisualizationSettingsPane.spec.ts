faa31879f0ab1d9968aa21c9a469a4893365ffb8
Liam Mulhall <liam.mulhall@colorado.edu>
2022-06-07 14:02:14 -0600

    fix: Link to Issue

--- src/components/__tests__/VisualizationSettingsPane.spec.ts
+++ src/components/__tests__/VisualizationSettingsPane.spec.ts
@@ -6,7 +6,7 @@ describe('VisualizationSettingsPane', () => {
     /*
     As of 2022-05-16, I (Liam Mulhall) don't think VisualizationSettingsPane.vue
     can be tested. See:
-    https://github.com/numberscope/frontscope/blob/main/src/components/VizualizationSettingsPane.vue // eslint-disable-line
+    https://github.com/numberscope/frontscope/issues/117 // eslint-disable-line
 
     This file is a placeholder.
     */
