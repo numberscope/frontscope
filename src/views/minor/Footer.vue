da1e974e528131245bafe95252f98ebe4769b348
Liam Mulhall <mulhall.edward.liam@gmail.com>
2022-07-18 15:21:03 -0600

    feat: add GitHub and Twitter links to footer

--- src/views/minor/Footer.vue
+++ src/views/minor/Footer.vue
@@ -7,6 +7,17 @@
             >
             over the years!
         </p>
+        <p>
+            <i class="bi bi-github"></i>&nbsp;
+            <a class="footer-link" href="https://github.com/numberscope"
+                >GitHub</a
+            >
+            <span id="linkSpacer"></span>
+            <i class="bi bi-twitter"></i>&nbsp;
+            <a class="footer-link" href="https://twitter.com/numberscoper"
+                >Twitter</a
+            >
+        </p>
     </footer>
 </template>
 
@@ -21,5 +32,8 @@
     }
     .footer-link:hover {
         color: white;
+    }
+    #linkSpacer {
+        padding: 0 2em;
     }
 </style>
