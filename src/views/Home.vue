c0f289be959a774f8330cee316105c917f2a4bfd
Liam Mulhall <mulhall.edward.liam@gmail.com>
2022-07-18 16:28:20 -0600

    refactor: remove visualizer descriptions from home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,63 +17,12 @@
                 >CC0 1.0</a
             >.
         </p>
-        <div>
-            <div class="row" id="visualizerInfo">
-                <div class="col">
-                    <img class="visualizerImg" :src="gameOfLife" />
-                    <h2 class="mt-4 visualizerTitle"><b>Game of Life</b></h2>
-                    <p class="orangeText">
-                        The Game of Life is a cellular automaton model. The
-                        playing field for Game of Life is a grid of cells.
-                        Each cell can be either alive or dead and has eight
-                        neighbors, those being the cells directly touching it
-                        horizontally, vertically, and diagonally. Certain
-                        rules dictate whether or not a given cell will be
-                        alive in the next generation.
-                    </p>
-                </div>
-                <div class="col">
-                    <img class="visualizerImg" :src="shiftCompare" />
-                    <h2 class="mt-4 visualizerTitle">
-                        <b>Shift Comparison</b>
-                    </h2>
-                    <p class="orangeText">
-                        The shift comparison picture of a<sub>n</sub> is
-                        formed by setting the pixel at position (x,y) to be
-                        white if a<sub>x</sub>=a<sub>x+y</sub>, and black
-                        otherwise. The picture captures how a<sub>n</sub>
-                        repeats. Many interesting sequences do not repeat, but
-                        by taking the sequence mod n, that is, taking the
-                        remainders after dividing by n, we get sequences that
-                        do repeat.
-                    </p>
-                </div>
-                <div class="col">
-                    <img class="visualizerImg" :src="turtle" />
-                    <h2 class="mt-4 visualizerTitle">
-                        <b>Turtle Graphics</b>
-                    </h2>
-                    <p class="orangeText">
-                        Imagine a small turtle sitting on a paper. It can move
-                        forward and rotate, and as it moves it leaves a trail
-                        of ink behind it. The visualization of integer
-                        sequences with turtle graphics can thus be constructed
-                        with parameters from the numbers of given sequences.
-                        Most integer sequences will produce mindless
-                        scribbling. Some produce surprising geometric figures.
-                    </p>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
 <script setup lang="ts">
     import {RouterLink} from 'vue-router'
     import background4 from '../assets/img/toolpage/background4.jpg'
-    import gameOfLife from '../assets/img/homepage/gof.png'
-    import shiftCompare from '../assets/img/homepage/sc.png'
-    import turtle from '../assets/img/homepage/turtle.png'
 </script>
 
 <style>
@@ -97,15 +46,6 @@
         margin-bottom: 2em;
         font-weight: bold;
         font-size: large;
-    }
-    #visualizerInfo {
-        background-color: #809fff;
-        color: white;
-        margin-top: 2em;
-        padding: 4em;
-    }
-    .visualizerImg {
-        width: 70%;
     }
     @media only screen and (max-width: 600px) {
         #background4 {
