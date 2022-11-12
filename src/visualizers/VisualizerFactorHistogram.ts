9cd01392e91278278d513d83557ae86cdb1ac0bc
Decos14 <112645349+Decos14@users.noreply.github.com>
2022-11-11 21:46:03 -0700

    Fixing the second round of PR review by Glen Whitney

--- src/visualizers/VisualizerFactorHistogram.ts
+++ src/visualizers/VisualizerFactorHistogram.ts
@@ -1,11 +1,10 @@
 import {VisualizerExportModule} from '@/visualizers/VisualizerInterface'
 import {VisualizerDefault} from './VisualizerDefault'
-import {natlog} from '../shared/math'
 
 /** md
 # Factor Histogram
 
-(example image should go here)
+""
 
 This visualizer takes the prime factorization of each element in the sequence
 and then creates a histogram of the number of elements of the sequence
@@ -16,13 +15,16 @@ the x-axis. And the height is how many elements are in each bin of X.
 **/
 
 class FactorHistogramVisualizer extends VisualizerDefault {
-    name = 'Histogram'
+    name = 'Factor Histogram'
 
     binSize = 1
     terms = 100
-    firstIndex = this.seq.first
+    firstIndex = NaN
 
     params = {
+        /** md
+- Bin Size: The size of each bin of the histogram.
+         **/
         binSize: {
             value: this.binSize,
             forceType: 'integer',
@@ -30,17 +32,27 @@ class FactorHistogramVisualizer extends VisualizerDefault {
             required: true,
         },
 
+        /** md
+- First Index: The first index to start getting the factors and forming the 
+               histogram from. If the first index is before the first term
+               of the series then the first term of the series will be used
+         **/
         firstIndex: {
-            value: this.firstIndex,
+            value: '' as string | number,
             forceType: 'integer',
             displayName: 'First Index',
-            required: true,
+            required: false,
         },
 
+        /** md
+- Number of Terms: The number of terms in the series after the first term.
+                   If this goes past the last term of the sequence it will
+                   show all terms of the sequence after the first index.
+         **/
         terms: {
             value: this.terms,
             forceType: 'integer',
-            displayName: 'How many terms of the series',
+            displayName: 'Number of Terms',
             required: true,
         },
     }
@@ -58,36 +70,31 @@ class FactorHistogramVisualizer extends VisualizerDefault {
 
     // Obtain the true first index
     startIndex(): number {
-        return Math.max(this.firstIndex, this.seq.first)
+        if (
+            typeof this.params.firstIndex.value === 'string'
+            || this.firstIndex < this.seq.first
+        ) {
+            return this.seq.first
+        } else {
+            return this.firstIndex
+        }
     }
 
     // Obtain the true number of terms
-    numTerms(): number {
+    endIndex(): number {
         return Math.min(this.terms + this.startIndex(), this.seq.last)
-    }
-
-    // Find the largest value of the sequence asked for
-    largestValue(): bigint {
-        let largest_value = this.seq.getElement(this.startIndex())
-        for (let i = this.startIndex(); i < this.numTerms(); i++) {
-            const value = this.seq.getElement(i)
-            if (value > largest_value) {
-                largest_value = value
-            }
-        }
-        return largest_value
     }
 
     // Create an array with the number of factor of
     // each element at the corresponding index of the array
     factorArray(): number[] {
         const factorArray = []
-        for (let i = this.startIndex(); i < this.numTerms(); i++) {
+        for (let i = this.startIndex(); i < this.endIndex(); i++) {
             let counter = 0
             const factors = this.seq.getFactors(i)
             if (factors) {
                 for (const [base, power] of factors) {
-                    if (base > 0) {
+                    if (base > 0n) {
                         counter += Number(power)
                     } else if (base === 0n) {
                         counter = 0
@@ -105,11 +112,11 @@ class FactorHistogramVisualizer extends VisualizerDefault {
     binFactorArray(): number[] {
         const binFactorArray = []
         const factorArray = this.factorArray()
-        for (
-            let i = 0;
-            i < natlog(this.largestValue()) / natlog(2) + 1;
-            i++
-        ) {
+        const largestValue = factorArray.reduce(
+            (a, b) => Math.max(a, b),
+            -Infinity
+        )
+        for (let i = 0; i < largestValue + 1; i++) {
             binFactorArray.push(0)
         }
 
@@ -123,68 +130,65 @@ class FactorHistogramVisualizer extends VisualizerDefault {
     // Create a number that represents how
     // many pixels wide each bin should be
     binWidth(): number {
-        const binFactorArray = this.binFactorArray()
-        let array_length = 0
-        for (let i = 0; i < binFactorArray.length; i++) {
-            if (binFactorArray[i] != 0) {
-                array_length = i
-            }
-        }
-        return (0.95 * this.sketch.width) / (array_length + 1)
+        return (0.95 * this.sketch.width) / this.binFactorArray().length
     }
 
     // Create a number that represents how many pixels high
     // each increase of one in the bin array should be
     height(): number {
         const binFactorArray = this.binFactorArray()
-        let greatest_value = binFactorArray[0]
-        for (let i = 0; i < binFactorArray.length; i++) {
-            if (binFactorArray[i] > greatest_value) {
-                greatest_value = binFactorArray[i]
-            }
-        }
+        const greatest_value = binFactorArray.reduce(
+            (a, b) => Math.max(a, b),
+            -Infinity
+        )
         return (0.95 * this.sketch.width) / greatest_value
     }
 
     draw() {
-        //these numbers provide the rgb values for the background color
+        // These numbers provide the rgb values for the background color
+        // This is light blue
         this.sketch.background(176, 227, 255)
+        this.sketch.textSize(0.02 * this.sketch.height) //set the text to the correct size
         const height = this.height()
         const binWidth = this.binWidth()
         const binFactorArray = this.binFactorArray()
         this.sketch.line(
-            0.025 * this.sketch.width,
-            10,
-            0.025 * this.sketch.width,
+            // Draws the y-axis
+            0.025 * this.sketch.width, // Creates a slight offset from the side of the canvas
+            0,
+            0.025 * this.sketch.width, // Creates a slight offset from the side of the canvas
             this.sketch.height
         )
         this.sketch.line(
+            // Draws the x-axis
             0,
-            0.975 * this.sketch.height,
+            0.975 * this.sketch.height, // Creates a slight offset from the side of the canvas
             this.sketch.width,
-            0.975 * this.sketch.height
+            0.975 * this.sketch.height // Creates a slight offset from the side of the canvas
         )
 
         for (let i = 0; i < binFactorArray.length; i++) {
             this.sketch.rect(
+                // Draws the rectangles for the Histogram
                 0.025 * this.sketch.width + binWidth * i,
                 0.975 * this.sketch.height - height * binFactorArray[i],
                 binWidth,
                 height * binFactorArray[i]
             )
             if (this.binSize != 1) {
+                // Draws text for if the bin size is not 1
                 this.sketch.text(
-                    this.binSize * i
-                        + ' - '
-                        + (i + this.binSize * i - i + this.binSize - 1),
-                    20 + (binWidth * (i + 1) - binWidth / 2),
-                    795
+                    this.binSize * i + ' - ' + (this.binSize * (i + 1) - 1),
+                    0.025 + binWidth * (i + 1 / 2),
+                    0.995 * this.sketch.width
                 )
             } else {
+                // Draws text for if the bin size is 1
                 this.sketch.text(
                     i,
-                    20 + (binWidth * (i + 1) - binWidth / 2),
-                    795
+                    0.025 * this.sketch.width
+                        + (binWidth * (i + 1) - binWidth / 2),
+                    0.995 * this.sketch.width
                 )
             }
         }
