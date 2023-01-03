89598f88f60e344fb611a51c28af0c9861c69a0d
Kate Stange <kstange@math.colorado.edu>
2023-01-02 18:15:18 -0700

    docs for grid-vis minus images

--- src/visualizers/VisualizerGrid.ts
+++ src/visualizers/VisualizerGrid.ts
@@ -3,6 +3,128 @@ import {VisualizerDefault} from '@/visualizers/VisualizerDefault'
 import {floorSqrt} from '@/shared/math'
 import type {ParamInterface} from '@/shared/Paramable'
 
+/** md
+# Grid Visualizer
+
+(example image should go here)
+
+This visualizer puts a sequence in a square spiral or in
+rows and allows you to highlight numbers based on various
+properties.
+
+The inspiration for this visualizer is [Ulam's spiral](https://en.wikipedia.org/wiki/Ulam_spiral), 
+which puts the natural numbers in a square spiral
+and highlights the primes.  One can also highlight properties such a
+whether a number is abundant or polygonal.  Several properties can be highlighted at once, 
+in which case later properties overcolor earlier ones.
+
+The original version of this visualizer was created by Olivia Brobin, as part of the [Experimental Mathematics Lab](https://www.colorado.edu/math/content/experimental-mathematics-lab) at [CU Boulder](https://www.colorado.edu/math/).
+
+## Examples
+
+###### 1 Natural numbers – Primes in a spiral with numbers
+
+These are the natural numbers in a square spiral, with each of the prime numbers highlighted; this is the classic [Ulam spiral](https://en.wikipedia.org/wiki/Ulam_spiral).  In this preset, the value of each term of the sequence is shown over the corresponding cell, in order to demonstrate the spiral.
+
+###### 2 Natural numbers – Primes in a spiral
+
+This shows many more terms; it becomes evident that the primes form long 
+diagonal lines. These diagonal lines are quadratic equations, namely x^2 + c, x^2 + 2x + c, x^2 -2x + c,
+and x^2 + 4x + c.
+
+###### 3 Natural numbers – Primes in rows with numbers
+
+The natural numbers are put in rows, with each of the prime numbers
+highlighted.
+
+###### 4 Primes in rows
+
+Explanation?
+
+###### 5 Natural numbers – Primes in rows that reset and augment with numbers
+
+These are the natural numbers put in rows, but each row the sequence resets with
+each number one more than the number above it.
+
+###### 6 Natural numbers – Primes in rows that reset and augment
+
+###### 7 Natural numbers – N^2 Primes in rows that reset and augment with numbers
+
+These are the square numbers put in rows, but each row the sequence resets with
+each number one more than the number above it.
+
+###### 8 Natural numbers – N^2 Primes in rows that reset and augment
+
+When there are lots of numbers, long diagonal lines become apparent. The diagonal
+lines that go up and to the right each have a corresponding diagonal line that goes down and to the right
+because numbers repeat when they are arranged this way. The quadratic equation for which each of these
+diagonals corresponds is x^2 – x + C
+
+###### 9 Natural numbers – Abundant numbers in spiral
+
+These are the natural numbers in a spiral. The abundant numbers are the black
+squares, and the non-abundant numbers are the white squares. Abundant numbers are numbers for which
+the sum of the factors is greater than the As you can see, the abundant numbers have a lot more structure
+than the abundant numbers.
+
+###### 10 Natural numbers – Abundant numbers in rows
+
+The structure of the abundant numbers becomes even more evident when they are
+put in rows.
+
+###### 11 Natural numbers – Abundant numbers and primes in spiral
+
+The abundant numbers are black, and the primes are red. When they are show
+together, primes apear less sporadic as they seems to fit around the abundant numbers.
+
+###### 12 Natural numbers – Polygonal Numbers in spiral
+
+This is the natural numbers in a spiral. The polygonal number are highlighted. The triangle
+numbers are red, the square number are orange, the pentagonal numbesr are yellow, the hexagonal
+numbers are green, the heptagonal numbers are blue, and the octagonal numbers are purple. Polygonal
+numbers are the number of dots that can be arranged in the shape of that polygon. For example, 6 is a
+triangle number because one can form a triangle with three dots at the bottom, two dots in the middle, and
+one dot at the top.
+
+###### 13 Natural numbers – Polyongal Number in rows
+
+These are the polygonal numbers highlighted in rows.
+
+###### 14 Natural numbers – N^2 Polygonal Numbers in rows that reset and augment
+
+These are the polygonal numbers highlighted in rows with the top row being x^2 and each
+of the squares being one more than the number above it.
+
+###### 15 Natural numbers, Rainbow Colored in rows with numbers
+
+These are the natural numbers in rows wiht each digit colored a different color.
+
+###### 16 Natural numbers, Rainbow Colored in spiral
+
+These are the natural numbers in a spiral with each digit colored a different color.
+
+###### 17 Pi ([A000796](https://oeis.org/A000796)) – Rainbow Colored in rows (With 1000 numbers)
+
+These are the digits of pi in rows, with each digit colored a different color.
+
+###### 18 Pi ([A000796](https://oeis.org/A000796)) – Rainbow Colored in rows (With 40000 numbers)
+
+These are the digits of pi in rows, with each digit colored a different color.
+
+###### 19 Abundant Numbers ([A005101](https://oeis.org/A005101)) Colored By Last Digit Alternative in a spiral
+
+When the abundant numbers are put in a spiral and highlighted by their last digit, the
+scarcit of odd abundant numbers becomes visually apparent. (With 1000 numbers)
+
+###### 20 Abundant Numbers ([A005101](https://oeis.org/A005101)) Colored By Last Digit Alternative in a spiral (With 40000 numbers)
+
+When the abundant numbers are put in a spiral and highlighted by their last digit, their
+structure clearly becomes not random.
+
+
+
+## Parameters
+**/
 const BLACK = '#000000'
 const WHITE = '#ffffff'
 
@@ -170,15 +292,6 @@ function getPropertyColor(value: string, index: number) {
 
 class VisualizerGrid extends VisualizerDefault {
     name = 'Grid'
-    description =
-        'This visualizer puts any number sequence in a spiral or in '
-        + 'rows and allows you to highlight numbers based on various'
-        + ' properties (later properties overcolor earlier ones).'
-        + '  One use of this tool is to put the natural numbers in a spiral'
-        + " and to highlight the primes, which is known as Ulam's spiral."
-        + '  Another use of this tool is highlighting properties such as'
-        + ' whether a number is abundant, polygonal, '
-        + 'or in some other sequence.'
 
     //Grid variables
     amountOfNumbers = 4096
@@ -211,6 +324,21 @@ class VisualizerGrid extends VisualizerDefault {
     ]
 
     params: {[key: string]: ParamInterface} = {
+        /** md
+### preset: Which preset to display
+
+If a preset other than ```Custom``` is selected, then the ```Properties``` portion of the dialog is overriden.
+For details on the meanings of the terms below, see the [properties section](#property-id) of the documentation.
+
+- Custom:  the remaining properties can be set by the user
+- Primes:  primes are shown in red 
+- Abundant_Numbers:  the abundant numbers are shown in black 
+- Abundant_Numbers_And_Primes:  the primes are shown in red and the abundant numbers in black
+- Polygonal_Numbers:  the polygonal numbers are shown in a variety of different colours (one for each type of polygon)
+- Color_By_Last_Digit_1:  the last digit is shown (one colour for each digit in a rainbow style)
+- Color_By_Last_Digit_2:  a variation on the last, where some digits are 'secondary' so the boxes are smaller
+
+         **/
         preset: {
             value: this.preset,
             from: Preset,
@@ -219,24 +347,49 @@ class VisualizerGrid extends VisualizerDefault {
             description:
                 'If a preset is selected, properties no longer function.',
         },
+
+        /** md
+### amountOfNumbers: The number of cells to display in the grid
+
+This may get laggy when it is in the thousands or higher, depending on the property being testing.
+
+         **/
         amountOfNumbers: {
             value: this.amountOfNumbers,
             displayName: 'Grid cells',
             required: false,
             description: 'Warning: display lags over 10,000 numbers',
         },
+
+        /** md
+### startingIndex: The sequence index at which to begin
+         **/
         startingIndex: {
             value: this.startingIndex,
             displayName: 'Starting Index',
             required: false,
             description: '',
         },
+
+        /** md
+### pathType: The path to follow while filling numbers into the grid.
+
+- Spiral:  An Ulam-type square spiral out from the origin.
+- Rows:  Left-to-right, top-to-bottom in rows. 
+- Rows_Offset:  The n-th row contains the sequence beginning at term n, read left-to-right.
+         **/
         pathType: {
             value: this.pathType,
             from: PathType,
             displayName: 'Path in grid',
             required: false,
         },
+
+        /** md
+### showNumbers: Whether to show sequence values overtop of grid boxes
+
+When this is selected, the number of cells in the grid will be limited to 400 even if the user chooses more.
+         **/
         showNumbers: {
             value: this.showNumbers,
             forceType: 'boolean',
@@ -244,6 +397,10 @@ class VisualizerGrid extends VisualizerDefault {
             required: false,
             description: 'When true, grid is limited to 400 cells',
         },
+
+        /** md
+### numberColor: The font color of displayed numbers
+         **/
         numberColor: {
             value: this.numberColor,
             forceType: 'color',
@@ -253,6 +410,9 @@ class VisualizerGrid extends VisualizerDefault {
             visiblePredicate: (dependentValue: boolean) =>
                 dependentValue === true,
         },
+        /** md
+### backgroundColor: The color of the background of the grid (non-highlighted boxes)
+         **/
         backgroundColor: {
             value: this.backgroundColor,
             forceType: 'color',
@@ -263,6 +423,25 @@ class VisualizerGrid extends VisualizerDefault {
 
     constructor() {
         super()
+        /** md
+### Property 1, 2, etc.:  Properties to display by colouring cells {#property-id}
+
+The user can add multiple properties.  For each, they must choosen a property to highlight, and a colour with which to display it.  For each, the user can select ```Primary_Color``` or ```Secondary_Color```.  Primary colours fill the complete cell, while secondary colours fill a smaller box centered in the cell; this allows for visualizing two properties at once without overcoloring.  Otherwise, later properties overcolor earlier ones.
+
+- None:  No effect
+- Prime:  Whether the absolute value of an integer is prime 
+- Negative:  Whether an integer is negative
+- Even:  Whether an integer is even
+- Divisible_By_Three, Divisible_By_Four etc.:  Whether an integer is divisible by 3, 4, etc.
+- Ends_With_Zero, Ends_With_One etc.:  Whether an integer's final digit is 0, 1, 2, etc.
+- Triangular_Number, Square_Number etc.:  Whether an integer is triangular, square, pentagonal etc.
+- Sum_Of_Two_Squares:  Whether an integer is a sum of two squares (always False for negative numbers)
+- Abundant:  Whether an integer is [abundant](https://en.wikipedia.org/wiki/Abundant_number), that is, exceeding the sum of its divisors (excluding itself)
+- Perfect:  Whether an integer is [perfect](https://en.wikipedia.org/wiki/Perfect_number), that is, equal to the sum of its divisors (excluding itself)
+- Deficient:  Whether an integer is [deficient](https://en.wikipedia.org/wiki/Deficient_number), that is, less than the sum of its divisors (excluding itself)
+- Semi_Prime:  Whether the absolute value of an integer is a [semi-prime](https://en.wikipedia.org/wiki/Semiprime), that is, a product of exactly two primes (possibly equal)
+         **/
+
         for (let i = 1; i < MAXIMUM_ALLOWED_PROPERTIES; i++) {
             this.propertyObjects.push({
                 property: Property.None,
