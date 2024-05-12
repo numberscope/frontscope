5cb772115f8c6c20a234468fc504ae4203c9f880
Max Derbenwick <M.A.Derbenwick@student.tudelft.nl>
2024-05-12 23:03:25 +0200

    Mild refactoring and new router setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,21 @@
-import Home from '../views/Home.vue'
-import Scope from '../views/Scope.vue'
+import HomeOld from '../views/Home.vue'
+import ScopeOld from '../views/Scope.vue'
+import HomeNew from '../views-new/Home.vue'
+import ScopeNew from '../views-new/Home.vue'
 import {createRouter, createWebHistory} from 'vue-router'
+
+const views =
+    import.meta.env.VITE_USE_NEW_UI === 'true'
+        ? {
+              // New views go here
+              home: HomeNew,
+              scope: ScopeNew,
+          }
+        : {
+              // Old views go here
+              home: HomeOld,
+              scope: ScopeOld,
+          }
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +23,12 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: Home,
+            component: views['home'],
         },
         {
             path: '/scope',
             name: 'scope',
-            component: Scope,
+            component: views['scope'],
         },
     ],
 })
