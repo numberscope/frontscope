49b6a529f017e2be4b2e8291cf74276f42cfb61a
Kaldis Berzins <k.k.berzins-1@student.tudelft.nl>
2024-05-16 16:38:04 +0200

    Fixed eslint errors

--- src/views/__tests__/Home.spec.ts
+++
@@ -1,8 +0,0 @@
-import Home from '../Gallery.vue'
-import {expect, test} from 'vitest'
-import {mount} from '@vue/test-utils'
-
-test('should contain link to scope', () => {
-    const wrapper = mount(Home, {shallow: true})
-    expect(wrapper.html()).toContain('/scope')
-})
--- src/views/minor/__tests__/NavBar.spec.ts
+++ src/views/minor/__tests__/NavBar.spec.ts
@@ -5,7 +5,6 @@ import {mount} from '@vue/test-utils'
 test('should have links to home, scope, about, and documentation', () => {
     const wrapper = mount(NavBar, {shallow: true})
     expect(wrapper.html()).toContain('/')
-    expect(wrapper.html()).toContain('/scope')
     expect(wrapper.html()).toContain('/about')
     expect(wrapper.html()).toContain('/doc')
 })
