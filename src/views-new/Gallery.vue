f57722e18cabb942e832b98c3fa23fc60be0263c
Gido Vitner <g.vitner@student.tudelft.nl>
2024-05-20 00:11:11 +0200

    Added a gallery template for desktop with fixed specimens

--- src/views-new/Gallery.vue
+++ src/views-new/Gallery.vue
@@ -3,12 +3,16 @@
         <div id="title-bar">
             <div id="titles">
                 <h2 id="title">Specimen gallery</h2>
-                <p id="visualizer-name">Self similarity telescope</p>
+                <p id="visualizer-name-mobile">Self similarity telescope</p>
             </div>
             <div id="change-button"></div>
         </div>
 
-        <div id="gallery">
+        <div class="visualizer-bar">
+            <p class="visualizer-name">Self similarity telescope</p>
+            <div class="arrow"></div>
+        </div>
+        <div class="gallery">
             <div class="specimen"></div>
             <div class="specimen"></div>
             <div class="specimen"></div>
@@ -24,6 +28,10 @@
 <script setup lang="ts"></script>
 
 <style>
+    p,
+    h2 {
+        margin: 0;
+    }
     #gallery-content {
         margin: 30px 20px;
     }
@@ -34,28 +42,59 @@
         align-items: center;
         margin-bottom: 30px;
     }
-    #title,
-    #visualizer-name {
-        margin: 0;
-    }
-
     #change-button {
         width: 100px;
         height: 100px;
         background-color: #000000;
     }
 
-    #gallery {
+    .visualizer-bar {
+        display: none;
+    }
+    .arrow {
+        width: 30px;
+        height: 30px;
+        background-color: #000000;
+    }
+
+    .gallery {
         display: grid;
-        //grid-template-columns: repeat(1, max-content);
-        //column-gap: 30px;
+        column-gap: 30px;
         row-gap: 30px;
-        //justify-content: center;
     }
 
     .specimen {
         width: 100%;
         height: 300px;
         background-color: #000000;
+    }
+
+    @media only screen and (min-width: 1200px) {
+        #gallery-content {
+            margin: 30px auto;
+        }
+
+        #visualizer-name-mobile,
+        #change-button {
+            display: none;
+        }
+
+        .visualizer-bar {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 20px;
+        }
+        .visualizer-name {
+            font-size: 1.5rem;
+        }
+
+        .gallery {
+            grid-template-columns: repeat(4, max-content);
+        }
+
+        .specimen {
+            width: 250px;
+        }
     }
 </style>
