e55dbfe802202b45664f74f3b9e77102478ed05b
mbavec <129102941+mbavec@users.noreply.github.com>
2024-06-03 00:01:04 +0200

    Gallery page (#341)
    Adds a route that shows a panel of thumbnails of multiple different specimens. This can be used, for example, for a curated list of interesting visualizers, or for an individual user's own visualizers.
    ---------
    Co-authored-by: Max Derbenwick <M.A.Derbenwick@student.tudelft.nl>
    Co-authored-by: maxd0328 <52940632+maxd0328@users.noreply.github.com>
    Co-authored-by: Kaldis Berzins <51906114+kaldis-berzins@users.noreply.github.com>
    Co-authored-by: mbavec <m.bavec@student.tudelft.nl>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
+    <NavBar />
     <main>
-        <NavBar />
         <RouterView />
     </main>
     <Footer />
@@ -12,29 +12,71 @@
     import {RouterView} from 'vue-router'
 </script>
 
+<!-- Global styles. This style tag is explicitly unscoped. -->
 <style>
     :root {
-        --ns-foreground: #2c3e50;
-        --ns-navigation-foreground: white;
-        --ns-navigation-background: #809fff;
-        --ns-information-foreground: white;
-        --ns-information-background: #3333ff;
-        --ns-footer-height: 9ex;
+        /* Font sizes */
+        --ns-size-body: 12px;
+        --ns-size-display: 24px;
+        --ns-size-heading: 16px;
+        --ns-size-subheading: 12px;
+        --ns-size-mini: 10px;
+        --ns-size-heading-2: 14px;
+        --ns-size-title: 24px;
+
+        /* Fonts */
+        --ns-font-display: 'Infinity', sans-serif;
+        --ns-font-main: 'Inter', sans-serif;
+
+        /* Colors */
+        --ns-color-primary: #809fff;
+        --ns-color-white: #ffffff;
+        --ns-color-light: #b5b5b5;
+        --ns-color-grey: #636363;
+        --ns-color-black: #2f2f2f;
+
+        /* Bolditude */
+        --ns-font-weight-medium: 500;
+
+        /* Breakpoint widths 
+        Default styles should be for vertical mobile devices
+        (devices narrower than --ns-breakpoint-mobile)
+
+        Small devices (landscape phones)
+        @media (min-width: var(--ns-breakpoint-mobile)) { ... }
+
+        Medium devices (tablets)
+        @media (min-width: var(--ns-breakpoint-tablet)) { ... }
+
+        Large devices (desktops)
+        @media (min-width: var(--ns-breakpoint-desktop)) { ... }
+        */
+        --ns-breakpoint-mobile: 580px;
+        --ns-breakpoint-tablet: 800px;
+        --ns-breakpoint-desktop: 1200px;
     }
-    main {
-        display: flex;
-        flex-direction: column;
-        min-height: calc(100vh - var(--ns-footer-height));
-        font-family: Arial, Helvetica, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        color: var(--ns-foreground);
+
+    * {
+        box-sizing: border-box;
+        font-family: var(--ns-font-main);
+        color: var(--ns-color-black);
     }
-    nav {
-        padding: 30px;
+
+    html,
+    body {
+        margin: 0;
+        padding: 0;
     }
-    nav a {
-        font-weight: bold;
-        color: var(--ns-foreground);
+
+    /* Display font */
+    @font-face {
+        font-family: 'Infinity';
+        src: url('/src/assets/fonts/infinity/Infinity.ttf');
+    }
+
+    /* Body font */
+    @font-face {
+        font-family: 'Inter';
+        src: url('/src/assets/fonts/inter/Inter-VariableFont_slnt\,wght.ttf');
     }
 </style>
