25d6986a37227545ac9af5236f3294dee804f184
Kaldis Berzins <kaldis.berzins@gmail.com>
2024-06-21 16:47:36 +0200

    Fixed configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /// <reference types="vitest" />
 
 import {defineConfig} from 'vite'
@@ -9,6 +10,14 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'happy-dom',
+        exclude: [
+            '**/node_modules/**',
+            '**/e2e/**',
+            '**/dist/**',
+            '**/cypress/**',
+            '**/.{idea,git,cache,output,temp}/**',
+            '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        ],
     },
     resolve: {
         alias: {
