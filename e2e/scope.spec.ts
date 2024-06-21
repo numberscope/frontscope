fe6cd3478d7b933f94e1e191bb3e3ac1a26ebd44
ZiggyBeijer <Z.Beijer@student.tudelft.nl>
2024-06-21 19:31:23 +0200

    Added extra end to end tests and added these tests to the pipeline

--- e2e/scope.spec.ts
+++ e2e/scope.spec.ts
@@ -1,4 +1,5 @@
 import {test, expect} from '@playwright/test'
+import {Specimen} from '../src/shared/Specimen'
 
 test.beforeEach(async ({page}) => {
     await page.goto('/')
@@ -68,33 +69,82 @@ test.describe('Scope', () => {
 
     test('Changing a sequence', async ({page}) => {
         await page.locator('#sequenceTab .change-button').click()
-
-        await page
-            .locator('#sequenceTab .buttons')
-            .dragTo(page.locator('#canvas-container'), {
-                sourcePosition: {
-                    x: 10,
-                    y: 10,
-                },
-
-                targetPosition: {
-                    x: 500,
-                    y: 100,
-                },
-            })
-
-        await expect(page.locator('#right-dropzone-container')).toHaveClass(
-            /empty/
-        )
+        await page.locator('#results .sequence').first().click()
         await expect(
-            page.locator('#right-dropzone-container .dropzone-resize')
-        ).toHaveCSS('display', 'none')
-
-        await expect(page.locator('#left-dropzone-container')).toHaveClass(
-            /empty/
-        )
-        await expect(
-            page.locator('#left-dropzone-container .dropzone-resize')
-        ).toHaveCSS('display', 'none')
+            await page.locator('#sequenceTab .subheading').innerText()
+        ).toMatch('Constant Sequence')
     })
+    test('minimizing a tab', async ({page}) => {
+        await page.locator('#visualiserTab .minimize').click()
+        await expect(page.locator('#visualiserTab')).toHaveClass(/minimized/)
+        await expect(
+            await page.locator('#visualiserTab').evaluate(element => {
+                return element.clientHeight
+            })
+        ).toBeLessThan(125)
+
+        await page.locator('#visualiserTab .minimize').click()
+        await expect(page.locator('#visualiserTab')).not.toHaveClass(
+            /minimized/
+        )
+        await expect(
+            await page.locator('#visualiserTab').evaluate(element => {
+                return element.clientHeight
+            })
+        ).toBeGreaterThan(375)
+    })
+    test('Changing a parameter', async ({page}) => {
+        const oldURL = page.url()
+
+        await page.locator('#modDimension').fill('100')
+        await expect(page.locator('#modDimension')).toHaveValue('100')
+
+        await expect(page.url()).not.toEqual(oldURL)
+    })
+    test('changing the specimen name', async ({page}) => {
+        const oldURL = page.url()
+
+        await page.locator('#specimen-bar-desktop input').fill('test')
+        const currentSpecimen = getCurrentSpecimen(page)
+
+        await expect(currentSpecimen.name).toEqual('test')
+        await expect(page.url()).not.toEqual(oldURL)
+    })
+    test('copying to clipboard', async ({page, context, browserName}) => {
+        // grant clipboard permissions for chromium, other browsers don't
+        // allow this due to privacy concerns
+        if (browserName === 'chromium') {
+            await context.grantPermissions([
+                'clipboard-read',
+                'clipboard-write',
+            ])
+        }
+
+        await page.locator('#specimen-bar-desktop #share-button').click()
+
+        let clipboardContent
+        if (browserName === 'chromium') {
+            const handle = await page.evaluateHandle(() =>
+                navigator.clipboard.readText()
+            )
+            clipboardContent = await handle.jsonValue()
+        } else {
+            // we can't read the clipboard content in other browsers
+            clipboardContent = page.url()
+        }
+
+        const url = page.url()
+        await expect(clipboardContent).toMatch(url)
+    })
+    function getCurrentSpecimen(page) {
+        const currentSpecimenIM = page.evaluate(() => {
+            return localStorage.getItem('currentSpecimen')
+        })
+        if (currentSpecimenIM === null) {
+            throw new Error('currentSpecimen is null')
+        }
+        const currentSpecimen = JSON.parse(currentSpecimenIM)
+        const specimen = Specimen.fromURL(currentSpecimen.url)
+        return specimen
+    }
 })
