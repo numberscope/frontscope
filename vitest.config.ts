f8c64d118cee654e34e1cc7cca98290c04a430c1
Max Derbenwick <M.A.Derbenwick@student.tudelft.nl>
2024-06-26 15:39:16 +0200

    Small testing fix for absolute imports

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,17 @@
 
 import {defineConfig} from 'vite'
 import Vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     plugins: [Vue()],
     test: {
         globals: true,
         environment: 'happy-dom',
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
 })
