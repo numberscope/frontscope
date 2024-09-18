acb49bbf6fce48b7bb7f8eab71714e4b2ff11150
Glen Whitney <glen@studioinfinity.org>
2024-09-18 13:05:15 -0700

    chore: Another try to grab ci snapshots

--- e2e/playci.config.ts
+++ e2e/playci.config.ts
@@ -4,11 +4,9 @@ import {baseConfiguration} from './playwright.config.ts'
 
 const dockerConfig = {
     ...baseConfiguration,
+    snapshotDir: baseConfiguration.testDir + '/ci_snaps',
     grepInvert: /@webGL/,
     timeout: 30000,
-    expect: {
-        toHaveScreenshot: {maxDiffPixelRatio: 0.03},
-    },
     reporter: [['list', {printSteps: true}]],
 }
 
