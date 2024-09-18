5214d32272800021a27d6fc664c2994cd69f43dd
Glen Whitney <glen@studioinfinity.org>
2024-09-18 12:56:13 -0700

    fix: oops test must succeed to generate artifact

--- e2e/playci.config.ts
+++ e2e/playci.config.ts
@@ -6,6 +6,9 @@ const dockerConfig = {
     ...baseConfiguration,
     grepInvert: /@webGL/,
     timeout: 30000,
+    expect: {
+        toHaveScreenshot: {maxDiffPixelRatio: 0.03},
+    },
     reporter: [['list', {printSteps: true}]],
 }
 
