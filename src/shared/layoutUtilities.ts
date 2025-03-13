5a98300adeb0148bc1af4746f6dad410fd7d9bcd
Glen Whitney <glen@studioinfinity.org>
2025-03-13 09:56:31 -0700

    fix: linebreak long numbers (in sequence mod)

--- src/shared/layoutUtilities.ts
+++ src/shared/layoutUtilities.ts
@@ -7,3 +7,22 @@ export function isMobile() {
     )
     return window.innerWidth < tabletBreakpoint
 }
+
+// Returns the decimal string of a big number with (breaking) thin spaces
+// every three characters if it is over nine digits long
+export function breakableString(n: bigint) {
+    let s = n.toString()
+    const minus = Number(n < 0) // stupid typescript, Number conversion auto
+    const breaklength = 10 + minus
+    if (s.length < breaklength) return s
+    let firstblock = s.length % 3
+    if (!firstblock && minus) firstblock = 4
+    let prefix = ''
+    if (firstblock) {
+        prefix = s.substr(0, firstblock)
+        s = s.substr(firstblock)
+    }
+    const parts = Array.from(s.match(/.{3}/g) ?? [])
+    if (prefix) parts.unshift(prefix)
+    return parts.join('\u2009') // thin space
+}
